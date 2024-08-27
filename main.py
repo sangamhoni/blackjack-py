b269236834187bc327e8ec762d7e1384915e8ef1
sangamhoni <87515320+subsangam@users.noreply.github.com>
2024-08-27 16:06:56 -0500

    Complete Adjust Aces Func

--- main.py
+++ main.py
@@ -39,7 +39,10 @@ def print_hands():
     # ie. hand_str = "Ace (11) + Queen (10) + 7 (7)"
     print(hand_str)
 
-
+# custom func to adjust Aces to 1 if it's a 'Bust'
+def adjust_ace(card_values):
+    while (sum(card_values)>21 and 11 in card_values):     # Check if total is over 21 and if there's an Ace valued at 11
+        card_values.index(11)=1     # Change the Ace's value from 11 to 1
 
 
 
@@ -47,4 +50,37 @@ def print_hands():
 hit_card()
 hit_card()
 
+game_over=False
+
+# while (not game_over):
+    
+
 print_hands()
+
+
+# if (my_card<0 or my_card>13):
+#     print("BAD CARD")
+# else:
+#     print(f"Your hand value is {my_card}")
+
+
+# Write code here to identify the value of each card.
+# my_card=int(input(""))
+# if (my_card<0 or my_card>13):
+#     print("BAD CARD")
+# elif (my_card==1):
+#     print("Your hand value is 11.")
+# elif (my_card>=10 and my_card<=13):
+#     print("Your hand value is 10.")
+# else:
+#     print(f"Your hand value is {my_card}.")
+
+
+# Write code here to print out the game outcome given a hand value.
+# hand_value=int(input())
+# if (hand_value==21):
+#     print("BLACKJACK!")
+# elif (hand_value<4 or hand_value>31):
+#     print("BAD HAND VALUE!")
+# elif (hand_value>21):
+#     print("BUST.")
